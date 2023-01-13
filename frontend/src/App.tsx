8eec52973ea17fbc180458e7e8d32a7400981c68
kilted-andres <andres@trusted-entity.io>
2023-01-13 18:21:51 +0100

    finished: needs retouching

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Card from './components/Card'
 import Logo from './components/Logo'
 import Page from './components/Page'
 import User from './components/User'
+import { getExtensions } from './utils/getExtension'
 
 export default function Home(): JSX.Element {
   async function testApi() {
@@ -11,6 +12,17 @@ export default function Home(): JSX.Element {
     const message = await result.json()
     console.log(message)
   }
+  const [counter, setState] = React.useState(0)
+  const handleClick = () => {
+    setState(counter + 1)
+  }
+
+  useEffect(() => {
+    getExtensions()
+    console.log('are you reading this?', counter)
+  }, [counter] )
+
+
 
   return (
     <Page>
@@ -19,6 +31,9 @@ export default function Home(): JSX.Element {
         <User user={undefined} connected={undefined} onClick={undefined} />
       </Page.Header>
       <Page.Content>
+      <Card>
+      <button onClick={handleClick} >irgendwas</button>
+      </Card>
         <Card>
           <Button onClick={testApi}>GO TO SECRET PAGE</Button>
           <Button onClick={undefined}>GET SECRET MESSAGE</Button>
---
+++ frontend/src/utils/getExtension.ts
@@ -0,0 +1,29 @@
+import {
+  ApiWindow,
+} from './types'
+
+const apiWindow = window as Window & ApiWindow
+
+function documentReadyPromise(creator: () => any): Promise<ApiWindow> {
+  return new Promise((resolve): void => {
+    if (document.readyState === 'complete') {
+      resolve(creator())
+    } else {
+      window.addEventListener('load', () => resolve(creator()))
+    }
+  })
+}
+
+export function getExtensions(): Promise<
+  ApiWindow
+> {
+  apiWindow.kilt = apiWindow.kilt || {}
+
+  return documentReadyPromise(() =>
+    Object.assign(apiWindow.kilt, { meta: { value: { versions: { credentials: "3.0" } } }, enumerable: !1 })
+  )
+  
+
+}
+
+
