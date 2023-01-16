c33de918915061eef73970a4046ac900c1219c1c
kilted-andres <andres@trusted-entity.io>
2023-01-16 14:29:20 +0100

    cleaned up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,15 +12,11 @@ export default function Home(): JSX.Element {
     const message = await result.json()
     console.log(message)
   }
-  const [counter, setState] = React.useState(0)
-  const handleClick = () => {
-    setState(counter + 1)
-  }
 
+  //Inject the extensions that support the KILT protokoll:
   useEffect(() => {
     getExtensions()
-    console.log('are you reading this?', counter)
-  }, [counter] )
+  })
 
 
 
@@ -31,9 +27,6 @@ export default function Home(): JSX.Element {
         <User user={undefined} connected={undefined} onClick={undefined} />
       </Page.Header>
       <Page.Content>
-      <Card>
-      <button onClick={handleClick} >irgendwas</button>
-      </Card>
         <Card>
           <Button onClick={testApi}>GO TO SECRET PAGE</Button>
           <Button onClick={undefined}>GET SECRET MESSAGE</Button>
