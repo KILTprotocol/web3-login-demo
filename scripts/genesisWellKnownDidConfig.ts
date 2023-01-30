7081483cf25054114f9ccdb2918754c5c70f1a1b
kilted-andres <andres@trusted-entity.io>
2023-01-30 17:34:02 +0100

    great batch

---
+++ scripts/genesisWellKnownDidConfig.ts
@@ -0,0 +1,105 @@
+import * as Kilt from '@kiltprotocol/sdk-js';
+import fs from 'fs';
+import { generateKeypairs } from './attester/generateKeyPairs';
+import { createCredential, createPresentation, selfAttestCredential, getDomainLinkagePresentation } from './wellKnownDIDConfiguration';
+import dotenv from 'dotenv';
+import { generateAccount } from './attester/generateAccount';
+
+async function main() {
+
+    // Fetch variables from .env file:
+    dotenv.config();
+
+    const dAppURI = process.env.ATTESTER_DID_URI as Kilt.DidUri ?? `did:kilt:4${'noURIEstablished'}` as Kilt.DidUri; // maybe createCompleFullDID(an account with money)
+    const domainOrigin = process.env.ORIGIN ?? 'no origin assiged';
+    const dAppMnemonic = process.env.ATTESTER_DID_MNEMONIC ?? 'failure';
+    const fundsMnemonic = process.env.ATTESTER_ACCOUNT_MNEMONIC ?? 'no Sugar Daddy';
+
+    console.log("dAppURI", dAppURI);
+    console.log("domainOrigin", domainOrigin);
+    console.log("dAppMnemonic", dAppMnemonic);
+    console.log("fundsMnemonic", fundsMnemonic, "\n");
+
+
+    //Connect to the webSocket. This tells the Kilt Api to wich node to interact, and ergo also the blockchain (Spiritnet or Peregrine)
+    let webSocket = process.env.WSS_ADDRESS;
+    if (webSocket) {
+        await Kilt.connect(webSocket);
+
+    } else {
+        throw new Error("You need to define on the .env the WebSocket you want to connect with.");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // First Step: Create a Claim 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // The claim has to be based on the Domain Linkage CType. This CType is fetched from the Blockchain on './wellKnownDIDConfiguration' and imported here as cTypeDomeinLinkage
+
+    const domainCredential = await createCredential(domainOrigin, dAppURI);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Second Step: Self-attesting the credential of this claim
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //A valid credential requires an attestation. Since the website wants to link itself to the DID just created, it has to self-attest the domain linkage credential, i.e., write the credential attestation on chain using the same DID it is trying to link to.
+
+    const dAppsDidKeys = generateKeypairs(dAppMnemonic);
+    const dappAccount = generateAccount(fundsMnemonic);
+
+    await selfAttestCredential(domainCredential, dAppsDidKeys.attestation, dappAccount);
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Third Step: Create a presentation for the attested credential
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // To use the newly attested credential, we need to derive a presentation from it to host on the dapp website.
+
+
+    // We need the KeyId of the AssertionMethod Key. There is only
+    // one AssertionMethodKey and its id is stored on the blockchain.
+    const didResolveResult = await Kilt.Did.resolve(dAppURI);
+    if (typeof didResolveResult?.document === 'undefined') {
+        throw new Error('DID must be resolvable (i.e. not deleted)');
+    }
+    const assertionMethodKeyId = didResolveResult.document.assertionMethod![0].id;
+
+
+    // to declare the SingCallBacks is a bit tricky. You either have to speficy the type of every return variable, or of the whole return.   
+
+    // const presentationSignCallback = async ({ data }: any) => ({
+    //     signature: assertionMethodKey.sign(data) as Uint8Array,
+    //     keyUri: `${dAppURI}${assertionMethodKeyId}` as Kilt.DidResourceUri,
+    //     keyType: assertionMethodKey.type as Kilt.DidVerificationKey['type']
+    // });
+
+    const presentationSignCallback = async ({ data }: any) => ({
+        signature: dAppsDidKeys.attestation.sign(data),
+        keyUri: `${dAppURI}${assertionMethodKeyId}`,
+        keyType: dAppsDidKeys.attestation.type
+    }) as Kilt.SignResponseData;
+
+    const domainCredentialPresentation = await createPresentation(
+        domainCredential,
+        presentationSignCallback
+    );
+
+
+    const wellKnownDidconfig = await getDomainLinkagePresentation(domainCredentialPresentation);
+
+    console.log(JSON.stringify(wellKnownDidconfig, null, 2));
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Fourth Step: Host the presentation in your web App
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    fs.writeFile('well-known-did-configuration.json', JSON.stringify(wellKnownDidconfig, null, 2), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+
+
+}
+
+main().then(() => { });
--- scripts/wellKnownDIDConfiguration.ts
+++ scripts/wellKnownDIDConfiguration.ts
@@ -1,15 +1,19 @@
-import {
-    Did,
-    CType,
-    Credential,
-    Claim,
-    SignCallback,
-    DidUri,
-    Utils,
-    ICredentialPresentation,
-    DidResourceUri,
-    connect,
-} from '@kiltprotocol/sdk-js';
+// import {
+//     Did,
+//     CType,
+//     Credential,
+//     Claim,
+//     SignCallback,
+//     DidUri,
+//     Utils,
+//     ICredentialPresentation,
+//     DidResourceUri,
+//     connect,
+//     ICredential,
+//     ConfigService,
+//     Attestation,
+// } from '@kiltprotocol/sdk-js';
+import * as Kilt from '@kiltprotocol/sdk-js';
 import {
     CredentialSubject,
     DomainLinkageCredential,
@@ -28,7 +32,7 @@ export const DID_CONFIGURATION_CONTEXT =
 export const DID_VC_CONTEXT = 'https://www.w3.org/2018/credentials/v1';
 export const KILT_CREDENTIAL_IRI_PREFIX = 'kilt:cred:';
 
-export const ctypeDomainLinkage = CType.fromProperties(
+export const ctypeDomainLinkage = Kilt.CType.fromProperties(
     'Domain Linkage Credential',
     {
         origin: {
@@ -38,11 +42,10 @@ export const ctypeDomainLinkage = CType.fromProperties(
 );
 
 export async function createCredential(
-    signCallback: SignCallback,
     origin: string,
-    didUri: DidUri
-): Promise<ICredentialPresentation> {
-    const fullDid = await Did.resolve(didUri);
+    didUri: Kilt.DidUri
+): Promise<Kilt.ICredential> {
+    const fullDid = await Kilt.Did.resolve(didUri);
 
     if (!fullDid?.document) {
         throw new Error('No Did found: Please create a Full DID');
@@ -58,13 +61,13 @@ export async function createCredential(
         origin,
     };
 
-    const claim = Claim.fromCTypeAndClaimContents(
+    const claim = Kilt.Claim.fromCTypeAndClaimContents(
         ctypeDomainLinkage,
         domainClaimContents,
         document.uri
     );
 
-    const credential = Credential.fromClaim(claim);
+    const credential = Kilt.Credential.fromClaim(claim);
 
     const assertionKey = document.assertionMethod?.[0];
 
@@ -74,30 +77,34 @@ export async function createCredential(
         );
     }
 
-    return Credential.createPresentation({
+    return credential;
+}
+
+export async function createPresentation(credential: Kilt.ICredential, signCallback: Kilt.SignCallback): Promise<Kilt.ICredentialPresentation> {
+    return Kilt.Credential.createPresentation({
         credential,
         signCallback,
     });
 }
 
 export async function getDomainLinkagePresentation(
-    credential: ICredentialPresentation,
+    credentialPresentation: Kilt.ICredentialPresentation,
     expirationDate: string = new Date(
         Date.now() + 1000 * 60 * 60 * 24 * 365 * 5
     ).toISOString()
 ): Promise<VerifiableDomainLinkagePresentation> {
-    const claimContents = credential.claim.contents;
-    if (!credential.claim.owner && !claimContents.origin) {
+    const claimContents = credentialPresentation.claim.contents;
+    if (!credentialPresentation.claim.owner && !claimContents.origin) {
         throw new Error('Claim do not content an owner or origin');
     }
 
-    Did.validateUri(credential.claim.owner);
+    Kilt.Did.validateUri(credentialPresentation.claim.owner);
 
-    const didUri = credential.claim.owner;
+    const didUri = credentialPresentation.claim.owner;
 
     let origin: string;
     if (typeof claimContents.origin !== 'string') {
-        throw new Error('claim contents id is not a string');
+        throw new Error('claim contents origin is not a string');
     } else if (!validUrl.isUri(claimContents.origin)) {
         throw new Error('The claim contents origin is not a valid url');
     } else {
@@ -107,21 +114,38 @@ export async function getDomainLinkagePresentation(
     const credentialSubject = {
         id: didUri,
         origin,
-        rootHash: credential.rootHash,
+        rootHash: credentialPresentation.rootHash,
     };
+
+    // assuere that the credential is self attested 
+    const api = Kilt.ConfigService.get('api');
+    const encodedAttestationDetails = await api.query.attestation.attestations(
+        credentialPresentation.rootHash
+    );
+    const issuer = Kilt.Attestation.fromChain(
+        encodedAttestationDetails,
+        credentialPresentation.claim.cTypeHash
+    ).owner;
+
+    if (issuer !== didUri) {
+        throw new Error('the well-known-did should be self attested.');
+    }
 
     const issuanceDate = new Date().toISOString();
 
-    const { claimerSignature, rootHash } = credential;
+    const { claimerSignature, rootHash } = credentialPresentation;
 
-    //const id = toCredentialIRI(credential.rootHash);
-    const id = credential.rootHash;
+    if (!claimerSignature) {
+        throw new Error('Claimer signature is required.');
+    }
 
-    await Did.verifyDidSignature({
+    const id = rootHash;
+
+    await Kilt.Did.verifyDidSignature({
         expectedVerificationMethod: 'assertionMethod',
         signature: hexToU8a(claimerSignature.signature),
         keyUri: claimerSignature.keyUri,
-        message: Utils.Crypto.coToUInt8(rootHash),
+        message: Kilt.Utils.Crypto.coToUInt8(rootHash),
     });
 
     // add self-signed proof
@@ -133,7 +157,7 @@ export async function getDomainLinkagePresentation(
         challenge: claimerSignature.challenge,
     };
 
-    return {
+    const wellKnownDidconfig = {
         '@context': DID_CONFIGURATION_CONTEXT,
         linked_dids: [
             {
@@ -154,7 +178,9 @@ export async function getDomainLinkagePresentation(
                 proof,
             },
         ],
-    };
+    } as VerifiableDomainLinkagePresentation;
+
+    return wellKnownDidconfig;
 }
 
 async function asyncSome(
@@ -164,8 +190,72 @@ async function asyncSome(
     await Promise.all(credentials.map((credential) => verify(credential)));
 }
 
+/**
+ * A valid credential requires an attestation. 
+ * Since the website wants to link itself to the DID just created, it has to self-attest the domain linkage credential, i.e., write the credential attestation on chain using the same DID it is trying to link to.
+ * 
+ * @param credential - Credential from the claim to be attested. 
+ * @param attestationKey - Attestation Keypair from the DID of the dApp.
+ * @param payerAccount - Account paying for the transaction (attestation)
+ */
+export async function selfAttestCredential(credential: Kilt.ICredential, attestationKey: Kilt.KiltKeyringPair, payerAccount: Kilt.KiltKeyringPair) {
+
+    const api = Kilt.ConfigService.get('api');
+
+    // In order to attest the credential we go through the following steps:
+
+    // Step 1: calculating the claim hash
+
+    const { cTypeHash, claimHash } = Kilt.Attestation.fromCredentialAndDid(
+        credential,
+        credential.claim.owner
+    );
+
+    // Step 2:  creating the attest transaction
+
+    const attestationTx = api.tx.attestation.add(claimHash, cTypeHash, null);
+
+    // Step 3: authorizing the transaction with your DID
+    // We authorize the call using the attestation key of the Dapps DID.
+
+    const assertionMethodKey = attestationKey; //just for you to see the two synonims.
+
+    let submitTx: Kilt.SubmittableExtrinsic;
+
+    const signCallback = async ({ data }: any) => ({
+        signature: assertionMethodKey.sign(data),
+        keyType: assertionMethodKey.type
+    });
+
+    // Step 4: paying for the transaction with a KILT account and submitting it to the chain
+
+    try {
+        submitTx = await Kilt.Did.authorizeTx(
+            credential.claim.owner,
+            attestationTx,
+            signCallback,
+            payerAccount.address
+        );
+    } catch (error) {
+        console.log("Failed to fetch my dApp's DID-Document");
+        throw new Error("Could not sing the self-attestation of the credential");
+
+    }
+
+    // Since DIDs can not hold any balance, we pay for the transaction using our blockchain account
+    const result = await Kilt.Blockchain.signAndSubmitTx(submitTx, payerAccount);
+
+    if (result.isError) {
+        console.log('Attestation failed');
+    } else {
+        console.log('Attestation successful');
+    }
+
+}
+
+
 export async function verifyDidConfigPresentation(
-    didUri: DidUri,
+    didUri: Kilt.DidUri,
     domainLinkageCredential: VerifiableDomainLinkagePresentation,
     origin: string
 ): Promise<void> {
@@ -178,7 +268,7 @@ export async function verifyDidConfigPresentation(
         const matchesSessionDid = didUri === credentialSubject.id;
         if (!matchesSessionDid) throw new Error('session did doesnt match');
 
-        Did.validateUri(credentialSubject.id);
+        Kilt.Did.validateUri(credentialSubject.id);
         const matchesIssuer = issuer === credentialSubject.id;
         if (!matchesIssuer) throw new Error('does not match the issuer');
 
@@ -186,7 +276,7 @@ export async function verifyDidConfigPresentation(
         if (!matchesOrigin) throw new Error('does not match the origin');
         if (!validUrl.isUri(origin)) throw new Error('not a valid uri');
 
-        const fullDid = await Did.resolve(didUri);
+        const fullDid = await Kilt.Did.resolve(didUri);
 
         if (!fullDid?.document) {
             throw new Error('No Did found: Please create a Full DID');
@@ -202,11 +292,11 @@ export async function verifyDidConfigPresentation(
         const rootHash = id;
         //const rootHash = fromCredentialIRI(id); //old
 
-        await Did.verifyDidSignature({
+        await Kilt.Did.verifyDidSignature({
             expectedVerificationMethod: 'assertionMethod',
             signature: hexToU8a(credential.proof.signature),
-            keyUri: credential.proof.verificationMethod as DidResourceUri,
-            message: Utils.Crypto.coToUInt8(rootHash),
+            keyUri: credential.proof.verificationMethod as Kilt.DidResourceUri,
+            message: Kilt.Utils.Crypto.coToUInt8(rootHash),
         });
     });
 }
