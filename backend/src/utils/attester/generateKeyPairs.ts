c8e408d494315c80e29c1f01beaca589dcc04f1b
kilted-andres <andres@trusted-entity.io>
2023-02-10 16:11:44 +0100

    fix: updated the creation of keys

--- backend/src/utils/attester/generateKeyPairs.ts
+++ backend/src/utils/attester/generateKeyPairs.ts
@@ -1,29 +1,58 @@
-import { mnemonicToMiniSecret } from '@polkadot/util-crypto';
-
+import {
+    mnemonicToMiniSecret,
+    sr25519PairFromSeed,
+    keyExtractPath,
+    keyFromPath,
+    blake2AsU8a
+} from '@polkadot/util-crypto';
 import * as Kilt from '@kiltprotocol/sdk-js';
+// import generateAccount from './generateAccount';
+
+
+function generateKeyAgreement(mnemonic: string) {
+    const secretKeyPair = sr25519PairFromSeed(mnemonicToMiniSecret(mnemonic));
+    const { path } = keyExtractPath('//did//keyAgreement//0');
+    const { secretKey } = keyFromPath(secretKeyPair, path, 'sr25519');
+    return Kilt.Utils.Crypto.makeEncryptionKeypairFromSeed(blake2AsU8a(secretKey));
+}
 
 export default function generateKeypairs(mnemonic: string) {
 
-    // Currently, the default the keytype used by the Kilt-team is "sr25519"
-    const authentication = Kilt.Utils.Crypto.makeKeypairFromSeed(
+    // Currently, the default the keytype used by the Kilt-team is "sr25519". Better to use it for compatibility.
+    const account = Kilt.Utils.Crypto.makeKeypairFromSeed(
         mnemonicToMiniSecret(mnemonic), "sr25519"
-    ).derive('//did');
-    const encryption = Kilt.Utils.Crypto.makeEncryptionKeypairFromSeed(
-        mnemonicToMiniSecret(mnemonic)
     );
-    const assertionMethod = authentication.derive(
-        '//attestation//0'
+    // Alternative using the other file
+    // const account = generateAccount(mnemonic);
+
+    // You can derive the keys however you want to and it will still work. 
+    // But if, for example, you try to load your seed phrase in a third party wallet, you will get a differnt set of keys, because the derivation is diferent. 
+    // For a start, it is better to use the same derivations as Sporran. So you can load your Accounts and DIDs there and check if everything worked fine. 
+
+    const authentication = account.derive(
+        '//did//0'
     ) as Kilt.KiltKeyringPair;
+
+    const assertionMethod = account.derive(
+        '//did//assertion//0'
+    ) as Kilt.KiltKeyringPair;
+
 
     // the delegation Keys are not needed for this project. 
-    const capabilityDelegation = authentication.derive(
-        '//delegation//0'
+    const capabilityDelegation = account.derive(
+        '//did//delegation//0'
     ) as Kilt.KiltKeyringPair;
 
-    const authentication0 = authentication.derive('//0') as Kilt.KiltKeyringPair;
+
+    // The encryption keys, a.k.a. keyAgreement, are not natively supported by the polkadot library. 
+    // So to derive this kinds of keys, we have to play a bit with lower-level details. 
+    // Thats whats done in the extra function generateKeyAgreement()
+
+    const keyAgreement = generateKeyAgreement(mnemonic);
+
     return {
-        authentication: authentication0,
-        keyAgreement: encryption,
+        authentication: authentication,
+        keyAgreement: keyAgreement,
         assertionMethod: assertionMethod,
         capabilityDelegation: capabilityDelegation
     };
