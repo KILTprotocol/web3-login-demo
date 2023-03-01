28f3f83425143790e6e4282e66afa7144605feac
kilted-andres <andres@trusted-entity.io>
2023-03-01 13:09:01 +0100

    feat: better explanations and a name section

--- scripts/genesisEnvironmentVariables.ts
+++ scripts/genesisEnvironmentVariables.ts
@@ -8,7 +8,13 @@ import { generateFullDid } from '../backend/src/utils/attester/generateFullDid';
 
 async function main() {
 
-    console.log("\u001B[38;5;201m", "Making this output purple");
+    console.log("\u001B[38;5;201m", "Making this output purple\n");
+    console.log(
+        "This is a script for an easy creation of the enviroment variables needed for your dApp's functionality.\n",
+        'All eviroment variables need to be saved on a file called ".env" that you need to create and save on the project\'s root directory.\n',
+        "Is it a standard that all enviroment variables are name with capitalized letters. Please, follow the standard.\n",
+        "Alternatively, you could create some of the environment values otherwise and let this script do the rest for you. Or (for pros) make them all otherwise and don't use this.\n\n"
+    );
 
     // try to read the variables from the .env-file:
     dotenv.config();
@@ -18,10 +24,11 @@ async function main() {
     const DAPP_ACCOUNT_ADDRESS = process.env.DAPP_ACCOUNT_ADDRESS; // fundsAddress
     const DAPP_DID_MNEMONIC = process.env.DAPP_DID_MNEMONIC; // dAppMnemonic
     const DAPP_DID_URI = process.env.DAPP_DID_URI; // dAppURI
+    const DAPP_NAME = process.env.DAPP_NAME; // Your dApp's name
 
-    // decide where you are: 
+    // figure out your project's current state: 
     let step: number = 0;
-    let stairs: (string | undefined)[] = [WSS_ADDRESS, ORIGIN, DAPP_ACCOUNT_MNEMONIC, DAPP_ACCOUNT_ADDRESS, DAPP_DID_MNEMONIC, DAPP_DID_URI];
+    let stairs: (string | undefined)[] = [WSS_ADDRESS, ORIGIN, DAPP_ACCOUNT_MNEMONIC, DAPP_ACCOUNT_ADDRESS, DAPP_DID_MNEMONIC, DAPP_DID_URI, DAPP_NAME];
 
     for (let index = 0; index < stairs.length; index++) {
         const value = stairs[index];
@@ -33,7 +40,8 @@ async function main() {
         } else if (index === stairs.length - 1) {
             step = stairs.length; // You already climbed up all the stairs
             //throw new Error("It seems like all enviorment variables are already defined.");
-            console.log("It seems like all enviorment variables are already defined.");
+            console.log("It seems like all enviorment variables are already defined.\n",
+                "Take in consideration, that this script does not verify if the environment values already defined are valid.\n\n");
         }
     }
 
@@ -41,7 +49,7 @@ async function main() {
     switch (step) {
         case 0: // first assign a websocket of the blockchain you want to interact with:
             console.log("\ntrouble reading the address of the WebSocket\n");
-            console.log("please, define a value for WSS_ADDRESS on the .env-file to continue, and then run this file again\n");
+            console.log("please, define a value for WSS_ADDRESS on the .env-file to continue\n");
             console.log("to connect to the KILT-Test-Blockchain, named Peregrine (recomended), please save the following:\n\n\n",
                 'WSS_ADDRESS=wss://peregrine.kilt.io/parachain-public-ws\n\n');
             console.log("In the future, if you wish to interact with the production KILT-Blockchain, named Spiritnet, change the address to a web-socket (public Endpoint) of Spiritnet.",
@@ -50,7 +58,7 @@ async function main() {
 
         case 1: // then assign where the dApp is going to be reachable
             console.log("\ntrouble reading the URL-Address of your dApp\n");
-            console.log("please, define a value for ORIGIN on the .env-file to continue, and then run this file again\n");
+            console.log("please, define a value for ORIGIN on the .env-file to continue\n");
             console.log("first it should only run locally. You can use a custom IP or just the default:\n");
             console.log("Default dApps domain origin: \n",
                 'ORIGIN=http://localhost:8080', "\n\n");
@@ -60,14 +68,13 @@ async function main() {
             console.log("\ntrouble reading the account's mnemonic of your dApp\n");
             console.log("An account is being generated for your dApp.");
             await Kilt.connect(WSS_ADDRESS!);
-            const mnemonic = mnemonicGenerate();
-            // you could manually enter a custom mnemonic here instead
+            // You could also pass a specific mnemonic, but here we generate a random mnemonic.
+            const mnemonic = mnemonicGenerate();// for costum, replace here with a string of 12 BIP-39 words
             const account = generateAccount(mnemonic);
             console.log('\n please, save mnemonic and address of your dApps account to the .env-file to continue!\n\n');
             console.log(`DAPP_ACCOUNT_MNEMONIC=${mnemonic}`);
             console.log(`DAPP_ACCOUNT_ADDRESS=${account.address}\n\n`);
-            console.log('You also need to deposit funds on this account, to be able to create a DID.\n For peregrine-accounts you can use: https://faucet.peregrine.kilt.io/');
-            console.log("\nAfterwards run this file again\n");
+            console.log(`You also need to deposit funds on this account, to be able to create a DID.\n For peregrine-accounts you can use: https://faucet.peregrine.kilt.io/?${account.address} \n\n`);
             await Kilt.disconnect();
             break;
 
@@ -77,8 +84,7 @@ async function main() {
             const accounty = generateAccount(DAPP_ACCOUNT_MNEMONIC!);
             console.log('\n please, save the address of your dApps account to the .env-file to continue!\n\n');
             console.log(`DAPP_ACCOUNT_ADDRESS=${accounty.address}\n\n`);
-            console.log('You also need to deposit funds on this account, to be able to create a DID.\n For peregrine-accounts you can use: https://faucet.peregrine.kilt.io/');
-            console.log("\nAfterwards run this file again\n");
+            console.log(`You also need to deposit funds on this account, to be able to create a DID.\n For peregrine-accounts you can use: https://faucet.peregrine.kilt.io/?${accounty.address} \n\n`);
             await Kilt.disconnect();
             break;
 
@@ -86,7 +92,7 @@ async function main() {
             // Load attester account
             console.log("\ntrouble reading the mnemonic of your dApp's DID\n");
             await Kilt.connect(WSS_ADDRESS!);
-            console.log("\n\nA decentralized identity (DID) is being generated for your dApp.\n\n");
+            console.log("\n\nA decentralized identity (DID) is trying to be generated for your dApp. This could take some minutes.\n Make sure you have enough funds! \n\n");
             const attesterAccount = generateAccount(DAPP_ACCOUNT_MNEMONIC!);
             // the DID can be generated by a different mnemonic than from the account. This is also the prefered option.
             // You could also pass a specific mnemonic, but here we generate a random mnemonic.
@@ -109,16 +115,24 @@ async function main() {
             if (!resolved?.metadata.canonicalId) {
                 console.log("there is no DID-Document on the chain for the given mnemonic. The corresponding Uri would be useless.");
                 console.log("Please, provide a mnemonic of registered DID!");
-                console.log("Easy way: delete current mnemonic and run this script again");
+                console.log("Easy way: delete current mnemonic from the .env-file and run this script again");
+                await Kilt.disconnect();
                 return;
             };
             console.log("\n please, save the URI of your dApp's DID to the .env-file to continue!\n");
             console.log(`DAPP_DID_URI=${dAppDidUri}\n`);
             await Kilt.disconnect();
             break;
+        case 6:
+            console.log("\ntrouble reading your dApp's Name\n");
+            console.log("Please provide a name inside the .env file using this constant name: ");
+            console.log(`DAPP_NAME={your name's here}\n`);
+
+            break;
     }
 
-    console.log("\u001b[0m", "reset output appareance");
+    console.log("If you are still missing some environment values and want the easy way, run this file again.\n");
+    console.log("\u001b[0m", "reset output's appareance");
     return;
 }
 main(); //run the code
