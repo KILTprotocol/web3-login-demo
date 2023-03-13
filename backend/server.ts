5f2ebd0abce0a6a7c6c22a9cc6b4bc3068d08f3c
kilted-andres <andres@trusted-entity.io>
2023-03-13 15:51:00 +0100

    fix: a bit prettier

--- backend/server.ts
+++ backend/server.ts
@@ -1,21 +1,21 @@
-import path from 'path';
+import path from 'path'
 
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
-import { generateSessionValues, verifySession } from './src/session/session';
+import { generateSessionValues, verifySession } from './src/session/session'
 
 // Letting the server know where the environment varibles are
-const projectRoootDirectory = path.dirname(__dirname);
-dotenv.config({ path: `${projectRoootDirectory}/.env` });
+const projectRoootDirectory = path.dirname(__dirname)
+dotenv.config({ path: `${projectRoootDirectory}/.env` })
 
-const app: Express = express();
-const PORT = process.env.PORT || 3000;
+const app: Express = express()
+const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use(
   cors({
@@ -25,17 +25,17 @@ app.use(
       `http://[::1]:${PORT}`
     ]
   })
-);
+)
 
 app.get('/api', (req: Request, res: Response) => {
-  console.log(`'/api' triggered`);
-  res.status(200).json('Welcome to the API for the KILT Web3 Login');
-});
+  console.log(`'/api' triggered`)
+  res.status(200).json('Welcome to the API for the KILT Web3 Login')
+})
 
-app.get('/api/session/start', generateSessionValues);
+app.get('/api/session/start', generateSessionValues)
 
-app.post('/api/session/verify', verifySession);
+app.post('/api/session/verify', verifySession)
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+})
--- backend/src/session/session.ts
+++ backend/src/session/session.ts
@@ -1,86 +1,86 @@
-import * as Kilt from '@kiltprotocol/sdk-js';
-import { Response, Request, NextFunction } from 'express';
-import { generateKeypairs } from '../utils/attester/generateKeyPairs';
-import { getApi } from '../utils/connection';
+import * as Kilt from '@kiltprotocol/sdk-js'
+import { Response, Request, NextFunction } from 'express'
+
+import { generateKeypairs } from '../utils/attester/generateKeyPairs'
+import { getApi } from '../utils/connection'
 
 // Object to store all session values on the memory-cache:
-const sessionStorage: { [key: string]: any; } = {};
+const sessionStorage: { [key: string]: any } = {}
 
 export async function generateSessionValues(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  console.log('creating session Values');
+  console.log('creating session Values')
   try {
-    await getApi(); // connects to the websocket of your, in .env, specified blockchain
+    await getApi() // connects to the websocket of your, in .env, specified blockchain
 
-    const DAPP_DID_URI = process.env.DAPP_DID_URI as Kilt.DidUri;
-    const dAppName = process.env.DAPP_NAME ?? 'Your dApp Name';
-    const DAPP_DID_MNEMONIC = process.env.DAPP_DID_MNEMONIC;
-    const { keyAgreement } = generateKeypairs(DAPP_DID_MNEMONIC as string);
+    const DAPP_DID_URI = process.env.DAPP_DID_URI as Kilt.DidUri
+    const dAppName = process.env.DAPP_NAME ?? 'Your dApp Name'
+    const DAPP_DID_MNEMONIC = process.env.DAPP_DID_MNEMONIC
+    const { keyAgreement } = generateKeypairs(DAPP_DID_MNEMONIC as string)
 
     if (!DAPP_DID_URI)
-      throw new Error("enter your dApp's DID URI on the .env-file first");
+      throw new Error("enter your dApp's DID URI on the .env-file first")
 
     // fetch the DID document from the blockchain
-    const resolved = await Kilt.Did.resolve(DAPP_DID_URI);
+    const resolved = await Kilt.Did.resolve(DAPP_DID_URI)
 
     // Assure this did has a document on chain
     if (resolved === null) {
-      throw new Error('DID could not be resolved');
+      throw new Error('DID could not be resolved')
     }
     if (!resolved.document) {
       throw new Error(
         'No DID document could be fetched from your given dApps URI'
-      );
+      )
     }
-    const didDocument = resolved.document;
+    const didDocument = resolved.document
     // If there the DID does not have any key agreement key, throw
     if (!didDocument.keyAgreement || !didDocument.keyAgreement[0]) {
       throw new Error(
         'The DID of your dApp needs to have an Key Agreement to comunicate. Go get one and register in on chain.'
-      );
+      )
     }
     if (!didDocument.authentication || !didDocument.authentication[0]) {
       throw new Error(
         'The DID of your dApp needs to have an authentification Key to sing stuff. Go get one and register in on chain.'
-      );
+      )
     }
 
     if (didDocument.keyAgreement[0].id !== keyAgreement.publicKey.toString()) {
-      throw new Error("The public key Agrrement does not match. ");
-
+      throw new Error('The public key Agrrement does not match. ')
     }
 
     // this basiclly says how are you going to encrypt:
     const dAppEncryptionKeyUri =
-      `${DAPP_DID_URI}${didDocument.keyAgreement[0].id}` as Kilt.DidResourceUri;
+      `${DAPP_DID_URI}${didDocument.keyAgreement[0].id}` as Kilt.DidResourceUri
 
     // Generate and store sessionID and challenge on the server side for the next step.
     // A UUID is a universally unique identifier, a 128-bit label. Here express as a string of a hexaheximal number.
-    const sessionID = Kilt.Utils.UUID.generate();
-    const challenge = Kilt.Utils.UUID.generate();
+    const sessionID = Kilt.Utils.UUID.generate()
+    const challenge = Kilt.Utils.UUID.generate()
 
     const sessionValues = {
       sessionID: sessionID,
       dAppName: dAppName,
       dAppEncryptionKeyUri: dAppEncryptionKeyUri,
       challenge: challenge
-    };
+    }
 
-    console.log(sessionValues);
+    console.log(sessionValues)
 
-    sessionStorage[sessionID] = sessionValues;
+    sessionStorage[sessionID] = sessionValues
 
     // You can see all sessions Values (including old ones) with this:
     // console.log('All sessions stored:', sessionStorage);
     // to reset this list restart the server
 
-    response.status(200).send(sessionValues);
+    response.status(200).send(sessionValues)
   } catch (error) {
     // print the possible error on the frontend
-    next(error);
+    next(error)
   }
 }
 
@@ -95,43 +95,43 @@ export async function verifySession(
     // console.log("body", request.body);
 
     // extract variables:
-    const { extensionSession: session, serverSessionID } = request.body;
-    const { encryptedChallenge, nonce } = session;
+    const { extensionSession: session, serverSessionID } = request.body
+    const { encryptedChallenge, nonce } = session
     // This varible has different name depending on the session version
-    let encryptionKeyUri: Kilt.DidResourceUri;
+    let encryptionKeyUri: Kilt.DidResourceUri
     // if session is type PubSubSessionV1
     if ('encryptionKeyId' in session) {
-      encryptionKeyUri = session.encryptionKeyId as Kilt.DidResourceUri;
+      encryptionKeyUri = session.encryptionKeyId as Kilt.DidResourceUri
     } else {
       // if session is type PubSubSessionV2
-      encryptionKeyUri = session.encryptionKeyUri;
+      encryptionKeyUri = session.encryptionKeyUri
     }
-    const encryptionKey = await Kilt.Did.resolveKey(encryptionKeyUri);
+    const encryptionKey = await Kilt.Did.resolveKey(encryptionKeyUri)
     if (!encryptionKey) {
-      throw new Error('an encryption key is required');
+      throw new Error('an encryption key is required')
     }
 
     // get your encryption Key, a.k.a. Key Agreement
-    const dAppDidMnemonic = process.env.DAPP_DID_MNEMONIC;
+    const dAppDidMnemonic = process.env.DAPP_DID_MNEMONIC
     if (!dAppDidMnemonic)
-      throw new Error('Enter your dApps mnemonic on the .env file');
+      throw new Error('Enter your dApps mnemonic on the .env file')
 
-    const { keyAgreement } = generateKeypairs(dAppDidMnemonic);
+    const { keyAgreement } = generateKeypairs(dAppDidMnemonic)
 
     const decryptedBytes = Kilt.Utils.Crypto.decryptAsymmetric(
       { box: encryptedChallenge, nonce },
       encryptionKey.publicKey,
       keyAgreement.secretKey // derived from your seed phrase
-    );
+    )
     // If it fails to decrypt, throw.
     if (!decryptedBytes) {
       throw new Error(
         'Could not decode/decrypt the challange from the extension'
-      );
+      )
     }
 
-    const decryptedChallenge = Kilt.Utils.Crypto.u8aToHex(decryptedBytes);
-    const originalChallenge = sessionStorage[serverSessionID].challenge;
+    const decryptedChallenge = Kilt.Utils.Crypto.u8aToHex(decryptedBytes)
+    const originalChallenge = sessionStorage[serverSessionID].challenge
 
     // Compare the decrypted challenge to the challenge you stored earlier.
     console.log(
@@ -140,23 +140,23 @@ export async function verifySession(
       '\n',
       'decrypted challenge: ',
       decryptedChallenge
-    );
+    )
     if (decryptedChallenge !== originalChallenge) {
       response
         .status(401)
-        .send("Session verification failed. The challenges don't match.");
-      throw new Error('Invalid challenge');
+        .send("Session verification failed. The challenges don't match.")
+      throw new Error('Invalid challenge')
     }
 
     response
       .status(200)
       .send(
         'Session succesfully verified. Extension and dApp understand each other.'
-      );
+      )
   } catch (err) {
     // print the possible error on the frontend
-    next(err);
+    next(err)
   }
 
-  return;
+  return
 }
