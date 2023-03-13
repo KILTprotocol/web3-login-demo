6f4e360cfc491eef216f18dee5e9e88d6f8be2be
kilted-andres <andres@trusted-entity.io>
2023-03-13 15:20:26 +0100

    fix: delete unused import

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,31 @@
-import dotenv from 'dotenv';
 import path from 'path';
-import express, { Express, Request, Response, Router } from 'express';
+
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+
 import { generateSessionValues, verifySession } from './src/session/session';
 
 // Letting the server know where the environment varibles are
 const projectRoootDirectory = path.dirname(__dirname);
 dotenv.config({ path: `${projectRoootDirectory}/.env` });
 
-
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
-
-
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(cors({ origin: [`http://localhost:${PORT}`, `http://127.0.0.1:${PORT}`, `http://[::1]:${PORT}`] }));
+app.use(
+  cors({
+    origin: [
+      `http://localhost:${PORT}`,
+      `http://127.0.0.1:${PORT}`,
+      `http://[::1]:${PORT}`
+    ]
+  })
+);
 
 app.get('/api', (req: Request, res: Response) => {
   console.log(`'/api' triggered`);
@@ -28,8 +35,6 @@ app.get('/api', (req: Request, res: Response) => {
 app.get('/api/session/start', generateSessionValues);
 
 app.post('/api/session/verify', verifySession);
-
-
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
