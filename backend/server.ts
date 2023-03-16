943ade20a07808324f94111e83bacdea07c96ede
kilted-andres <andres@trusted-entity.io>
2023-03-16 19:07:12 +0100

    feat: verify session of JWT

--- backend/server.ts
+++ backend/server.ts
@@ -4,11 +4,13 @@ import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+// import cookieParser from 'cookie-parser'
 
 import {
   sendSessionValues,
   verifySession,
-  generateJWT
+  generateJWT,
+  verifySessionJWT
 } from './src/session/session'
 
 // Letting the server know where the environment varibles are
@@ -21,6 +23,7 @@ const PORT = process.env.PORT || 3000
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// enable comunication between front and backend
 app.use(
   cors({
     origin: [
@@ -31,16 +34,21 @@ app.use(
   })
 )
 
+// app.use(cookieParser())
+
 app.get('/api', (req: Request, res: Response) => {
   console.log(`'/api' triggered`)
   res.status(200).json('Welcome to the API for the KILT Web3 Login')
 })
 
+// using a global array:
 app.get('/api/session/start', sendSessionValues)
 
-app.get('/api/session/jwt', generateJWT)
-
 app.post('/api/session/verify', verifySession)
+
+// using JWTs
+app.get('/api/session/startJWT', generateJWT)
+app.post('/api/session/verifyJWT', verifySessionJWT)
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
--- frontend/src/startExtensionSession.ts
+++ frontend/src/startExtensionSession.ts
@@ -3,6 +3,85 @@ import { getExtensions, apiWindow } from './utils/getExtension'
 export async function startExtensionSession() {
   getExtensions()
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Saving the session values as a JSON-Web-Token on a Cookie of the browser
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // generate and get a JasonWebToken with session values from the backend:
+  const serverSessionJWT = await fetch(`/api/session/startJWT`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      accessControlAllowOrigin: '*',
+      'Content-type': 'application/json',
+      Accept: 'application/json'
+    }
+  })
+  if (!serverSessionJWT.ok) throw Error(serverSessionJWT.statusText)
+
+  const payloadJWT = await serverSessionJWT.json()
+
+  console.log('Plain text accompanying the Cookie (Payload of JWT)', payloadJWT)
+
+  try {
+    // destructure the payload:
+    const { dAppName, dAppEncryptionKeyUri, challenge } = payloadJWT
+
+    const extensionSession = await apiWindow.kilt.sporran.startSession(
+      dAppName,
+      dAppEncryptionKeyUri,
+      challenge
+    )
+    console.log('the session was initialized (¬‿¬)')
+    console.log('session being returned by the extension:', extensionSession)
+
+    // Resolve the `session.encryptionKeyUri` and use this key and the nonce
+    // to decrypt `session.encryptedChallenge` and confirm that it’s equal to the original challenge.
+    // This verification must happen on the server-side.
+
+    const responseToBackend = JSON.stringify({
+      extensionSession,
+      serverSession: payloadJWT
+    })
+    // console.log("responseToBackend", responseToBackend);
+    await fetch(`/api/session/verifyJWT`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: responseToBackend
+    })
+
+    return extensionSession
+  } catch (error) {
+    console.error(
+      `Error verifying Session from:  ${apiWindow.kilt.sporran.name}: ${apiWindow.kilt.sporran.version},  ${error}`
+    )
+    throw error
+  }
+
+  // // Retrieve the JWT from the Cookie
+  //   const cookies: string[] = document.cookie
+  //     .split(';')
+  //     .map((cookie) => cookie.trim())
+  //   const jwtCookie: string | undefined = cookies.find((cookie) =>
+  //     cookie.startsWith('sessionJWT=')
+  //   )
+  //   const sessionJWT: string | null = jwtCookie ? jwtCookie.split('=')[1] : null
+
+  //   if (!sessionJWT)
+  //     throw new Error(
+  //       'No JSON-Web-Token with session values found on the cookies.'
+  //     )
+
+  //   console.log('The cookie crumbles: ', sessionJWT)
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Alternative: Not using JWT, but saving values in a array on the memory-cache
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   // generate and get session values from the backend:
   const serverSessionValues = await fetch(`/api/session/start`, {
     method: 'GET',
@@ -26,46 +105,6 @@ export async function startExtensionSession() {
     `dAppName: ${dAppName} \n`,
     `dAppEncryptionKeyUri: ${dAppEncryptionKeyUri} \n`
   )
-
-  // generate and get a JasonWebToken with session values from the backend:
-  const serverSessionJWT = await fetch(`/api/session/jwt`, {
-    method: 'GET',
-    credentials: 'include',
-    headers: {
-      accessControlAllowOrigin: '*',
-      'Content-type': 'application/json',
-      Accept: 'application/json'
-    }
-  })
-  if (!serverSessionJWT.ok) throw Error(serverSessionJWT.statusText)
-
-  console.log(
-    'The Json Web Token obtained from the backend is: ',
-    serverSessionJWT.body
-  )
-
-  // Retrieve the JWT from the Cookie and tell the extension to start the session:
-  try {
-    const cookies: string[] = document.cookie
-      .split(';')
-      .map((cookie) => cookie.trim())
-    const jwtCookie: string | undefined = cookies.find((cookie) =>
-      cookie.startsWith('sessionJWT=')
-    )
-    const sessionJWT: string | null = jwtCookie ? jwtCookie.split('=')[1] : null
-
-    if (!sessionJWT)
-      throw new Error(
-        'No JSON-Web-Token with session values found on the cookies.'
-      )
-
-    console.log('The cookie crumbles: ', sessionJWT)
-  } catch (error) {
-    console.error(
-      `Error initializing ${apiWindow.kilt.sporran.name}: ${apiWindow.kilt.sporran.version},  ${apiWindow.kilt.error}`
-    )
-    throw error
-  }
 
   try {
     const extensionSession = await apiWindow.kilt.sporran.startSession(
@@ -98,7 +137,7 @@ export async function startExtensionSession() {
     return extensionSession
   } catch (error) {
     console.error(
-      `Error initializing ${apiWindow.kilt.sporran.name}: ${apiWindow.kilt.sporran.version},  ${apiWindow.kilt.error}`
+      `Error initializing ${apiWindow.kilt.sporran.name}: ${apiWindow.kilt.sporran.version},  ${error}`
     )
     throw error
   }
