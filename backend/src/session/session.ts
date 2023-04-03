ad062492e50cf639805fd61f346eecc31bbb76b5
kilted-andres <andres@trusted-entity.io>
2023-04-03 10:49:43 +0200

    fix: rename the JWT-Secret Key

--- backend/src/session/session.ts
+++ backend/src/session/session.ts
@@ -56,10 +56,10 @@ export async function generateJWT(
     const payload = await generateSessionValues(
       request.app.locals.dappDidDocument
     )
-    const secretKey = process.env.JWT_ENCODER
+    const secretKey = process.env.JWT_UNIQUE_PEN
     if (!secretKey) {
       throw new Error(
-        "Define a value for 'JWT_ENCODER' on the '.env'-file first!"
+        "Define a value for 'JWT_UNIQUE_PEN' on the '.env'-file first!"
       )
     }
 
@@ -109,7 +109,7 @@ export async function verifySession(
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const secretKey = process.env.JWT_ENCODER
+  const secretKey = process.env.JWT_UNIQUE_PEN
   if (!secretKey) {
     response
       .status(500)
@@ -117,7 +117,7 @@ export async function verifySession(
         `Could not find JWT-Secret-key; so it is not possible to verify session.`
       )
     throw new Error(
-      "Define a value for 'JWT_ENCODER' on the '.env'-file first!"
+      "Define a value for 'JWT_UNIQUE_PEN' on the '.env'-file first!"
     )
   }
 
@@ -237,10 +237,10 @@ export async function getSessionJWT(
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const secretKey = process.env.JWT_ENCODER
+  const secretKey = process.env.JWT_UNIQUE_PEN
   if (!secretKey) {
     throw new Error(
-      "Define a value for 'JWT_ENCODER' on the '.env'-file first!"
+      "Define a value for 'JWT_UNIQUE_PEN' on the '.env'-file first!"
     )
   }
 
--- scripts/genesisEnvironmentVariables.ts
+++ scripts/genesisEnvironmentVariables.ts
@@ -26,7 +26,7 @@ const {
   // This should be a custom name for your dApp
   DAPP_NAME,
   // This is secret key (string) that encodes the Json-Web-Tokens before saving them in the Cookies
-  JWT_ENCODER
+  JWT_UNIQUE_PEN
 } = process.env
 
 async function main() {
@@ -54,7 +54,7 @@ async function main() {
     DAPP_DID_MNEMONIC,
     DAPP_DID_URI,
     DAPP_NAME,
-    JWT_ENCODER
+    JWT_UNIQUE_PEN
   ]
 
   // find the first element in the array "stairs" that it is still undefined.
@@ -251,8 +251,8 @@ function imploreName() {
 function imploreJwtSecretKey() {
   console.log(
     '\ntrouble reading the Secret Key your dApps use to encode the JSON-Web-Tokens\n',
-    `Please provide a string value for 'JWT_ENCODER' inside the .env file. \n`,
-    `JWT_ENCODER={Oh my God, you are so cryptic!}\n`
+    `Please provide a string value for 'JWT_UNIQUE_PEN' inside the .env file. \n`,
+    `JWT_UNIQUE_PEN={Oh my God, you are so cryptic!}\n`
   )
 }
 
