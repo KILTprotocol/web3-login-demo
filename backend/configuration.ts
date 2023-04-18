8e3f85b853de5afd2502811a3d5c0f69b9149819
kilted-andres <andres@trusted-entity.io>
2023-04-18 19:15:45 +0200

    feat: borrow config.ts from dudley

---
+++ backend/configuration.ts
@@ -0,0 +1,16 @@
+import path from 'path'
+import dotenv from 'dotenv'
+
+import * as Kilt from '@kiltprotocol/sdk-js'
+
+dotenv.config()
+
+// Letting the server know where the environment varibles are
+const projectRootDirectory = path.dirname(__dirname)
+dotenv.config({ path: `${projectRootDirectory}/.env` })
+
+export const PORT = process.env.PORT || 3000
+export const WSS_ADDRESS = process.env.WSS_ADDRESS || 'wss://peregrine.kilt.io'
+export const DAPP_DID_URI = process.env.DAPP_DID_URI as Kilt.DidUri
+export const DAPP_NAME = process.env.DAPP_NAME ?? 'Your dApp Name'
+export const DAPP_DID_MNEMONIC = process.env.DAPP_DID_MNEMONIC as string
