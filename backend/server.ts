64af07e06f2a917ef7809fa03e149d0b081a4547
kilted-andres <andres@trusted-entity.io>
2023-04-20 12:14:01 +0200

    feat: dudleys server stuff

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,4 @@
-import path from 'path'
-
-import dotenv from 'dotenv'
+import * as Kilt from '@kiltprotocol/sdk-js'
 import express, { Express, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
@@ -10,13 +8,16 @@ import { startSession } from './src/session/startSession'
 import { verifySession } from './src/session/verifySession'
 
 import { fetchDidDocument } from './src/utils/fetchDidDocument'
+import { getRequestCredential } from './src/getRequestCredential/getRequestCredential'
+import { postSubmitCredential } from './src/postSubmitCredential/postSubmitCredential'
+// Getting necessary environment constants:
+import { WSS_ADDRESS, PORT } from './configuration'
 
-// Letting the server know where the environment variables are
-const projectRootDirectory = path.dirname(__dirname)
-dotenv.config({ path: `${projectRootDirectory}/.env` })
+// // Letting the server know where the environment variables are
+// const projectRootDirectory = path.dirname(__dirname)
+// dotenv.config({ path: `${projectRootDirectory}/.env` })
 
 const app: Express = express()
-const PORT = process.env.PORT || 3000
 
 // Activating Middleware:
 
@@ -46,7 +47,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next()
 })
 
-// Setting GET and POST functions
+// Setting GET (requests) and POST (responses) functions
 
 app.get('/api', (req: Request, res: Response) => {
   res.status(200).json('Welcome to the API for the KILT Web3 Login')
@@ -54,7 +55,6 @@ app.get('/api', (req: Request, res: Response) => {
 
 // manage Session:
 
-// Login:
 // Starts the session from server side.
 app.get('/api/session/start', (req, res, next) =>
   startSession(req, res).catch(next)
@@ -63,6 +63,12 @@ app.get('/api/session/start', (req, res, next) =>
 app.post('/api/session/verify', (req, res, next) =>
   verifySession(req, res).catch(next)
 )
+
+// Manage Credentials:
+
+app.get('/api/getRequestCredential', getRequestCredential)
+app.post('/api/postSubmitCredential', postSubmitCredential)
+
 // We need the DID Document of the dApps DID (DAPP_DID_URI) before we can handle login requests.
 // We therefore start the server only after the document was fetched.
 fetchDidDocument()
@@ -76,3 +82,20 @@ fetchDidDocument()
   .catch((error) => {
     console.log(`Could not start server! ${error}`)
   })
+
+// connect to the api
+initializeServer()
+
+async function initializeServer() {
+  try {
+    await Kilt.connect(WSS_ADDRESS)
+    console.log(
+      `🔗[websocket]: Connected to WebSocket server at ${WSS_ADDRESS}`
+    )
+  } catch (error) {
+    console.error(
+      `❌[websocket]: Failed to connect to WebSocket server at ${WSS_ADDRESS}`,
+      error
+    )
+  }
+}
