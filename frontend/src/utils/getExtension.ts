4d9723c41138bea8fc6a9da0cff335b8f01daa6f
kilted-andres <andres@trusted-entity.io>
2023-04-28 18:30:57 +0200

    docs: show that docs are nasty

--- frontend/src/utils/getExtension.ts
+++ frontend/src/utils/getExtension.ts
@@ -10,6 +10,18 @@ export function getExtensions(): () => void {
     enumerable: false
   })
 
+  // This is the version recommended on the docs, but it breaks things:
+  // link: https://docs.kilt.io/docs/develop/dApp/session/
+
+  // Object.defineProperty(apiWindow.kilt, 'meta', {
+  //   value: {
+  //     versions: {
+  //       credentials: '3.0'
+  //     }
+  //   },
+  //   enumerable: false
+  // })
+
   apiWindow.dispatchEvent(new CustomEvent('kilt-dapp#initialized'))
   apiWindow.addEventListener('kilt-extension#initialized', getExtensions)
   return () =>
