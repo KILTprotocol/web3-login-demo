2e1345aa2dc18317c140321df9b83f0106e1332f
kilted-andres <andres@trusted-entity.io>
2023-04-28 17:59:14 +0200

    feat: dispatchEvent kilt-dapp#initialized

--- frontend/src/utils/getExtension.ts
+++ frontend/src/utils/getExtension.ts
@@ -2,12 +2,16 @@ import { ApiWindow } from './types'
 
 export const apiWindow = window as Window & ApiWindow
 
-export function getExtensions(): void {
+export function getExtensions(): () => void {
   apiWindow.kilt = apiWindow.kilt || {}
 
   Object.assign(apiWindow.kilt, {
     meta: { versions: { credentials: '3.0' } },
     enumerable: false
   })
-  apiWindow.addEventListener('kilt-dapp#initialized', getExtensions)
+
+  apiWindow.dispatchEvent(new CustomEvent('kilt-dapp#initialized'))
+  apiWindow.addEventListener('kilt-extension#initialized', getExtensions)
+  return () =>
+    apiWindow.removeEventListener('kilt-extension#initialized', getExtensions)
 }
