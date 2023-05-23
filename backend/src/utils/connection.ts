4bd2695af8697453e8799e11e7bd5af30155212f
kilted-andres <andres@trusted-entity.io>
2023-05-23 18:08:10 +0200

    fix: trust the config, no assertion

--- backend/src/utils/connection.ts
+++ backend/src/utils/connection.ts
@@ -14,12 +14,6 @@ export async function getApi() {
     return Kilt.ConfigService.get('api')
   }
 
-  // If it is not, connect to it using the Web-Socket Address from the environment variable:
-  if (!WSS_ADDRESS) {
-    throw new Error(
-      `please, define a value for 'WSS_ADDRESS' on the root's .env-file to use this function`
-    )
-  }
   // internally Kilt.connect() calls cryptoWaitReady()
   await Kilt.connect(WSS_ADDRESS)
   return Kilt.ConfigService.get('api')
