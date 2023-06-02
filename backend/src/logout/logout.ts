336cf9c45751dae14e69e2994dbeee87c2e4d3f1
kilted-andres <andres@trusted-entity.io>
2023-06-02 17:12:32 +0200

    feat: tiny comment

--- backend/src/logout/logout.ts
+++ backend/src/logout/logout.ts
@@ -7,6 +7,7 @@ export async function logout(
   request: Request,
   response: Response
 ): Promise<void> {
+  // Activate whatever custom action your dApp's backend should do on users logout here.
   deleteCredentialCookie(request, response)
   deleteSessionCookie(request, response)
   response.status(200).send('User has been logged out.')
