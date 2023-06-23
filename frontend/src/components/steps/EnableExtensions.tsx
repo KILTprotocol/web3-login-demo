6c75c2210e7da36550d8b280f920a846e088584a
kilted-andres <andres@trusted-entity.io>
2023-06-23 17:45:39 +0200

    feat: dehydrating

--- frontend/src/components/steps/EnableExtensions.tsx
+++ frontend/src/components/steps/EnableExtensions.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import styles from './Step.module.css'
-
-import RefreshButton from '../RefreshButton'
 
 export default function EnableExtensions() {
   const kiltExtensionsApiInitialized =
     typeof (window as any).kilt?.meta !== 'undefined'
-
-  const [availability, setAvailability] = useState(kiltExtensionsApiInitialized)
-
-  function refresh() {
-    const kiltExtensionsApiInitialized =
-      typeof (window as any).kilt?.meta !== 'undefined'
-    setAvailability(kiltExtensionsApiInitialized)
-  }
 
   return (
     <div className={styles.step}>
@@ -26,12 +16,8 @@ export default function EnableExtensions() {
         We perform this action when the website is loaded. The relevant code can
         be found in 'index.tsx'.
       </p>
-      <div className={styles.refresh_container}>
-        {availability && '✅ Extensions enabled'}
-        {!availability && '❌ Extensions not enabled'}
-
-        <RefreshButton action={refresh}></RefreshButton>
-      </div>
+      {kiltExtensionsApiInitialized && '✅ Extensions enabled'}
+      {!kiltExtensionsApiInitialized && '❌ Extensions not enabled'}
       <p>
         To verify this, you can execute 'window.kilt' in your browser's console.
       </p>
