9c003391a43f80cf735b4f848494b7ce6368e168
kilted-andres <andres@trusted-entity.io>
2023-06-23 15:58:50 +0200

    fix: rename extSessHelp

--- frontend/src/components/steps/StartSession.tsx
+++ frontend/src/components/steps/StartSession.tsx
@@ -15,8 +15,8 @@ interface Props {
 function StartSession({ extensionSession, setExtensionSession }: Props) {
   async function startSession() {
     console.log('trying to start the session! ')
-    const extSessHelp = await startExtensionSession()
-    setExtensionSession(extSessHelp)
+    const newSession = await startExtensionSession()
+    setExtensionSession(newSession)
   }
   // After startSession(), the Extension-Session-Values should be available for the backend. Done through cookie parser, (but data bank also possible).
   // The frontend still needs the Session-Object to be able to use its methods (functions). That's why we save on a React-State.
