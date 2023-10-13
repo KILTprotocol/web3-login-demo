6d80d8df7c6cc81213a7a0f5d0b99e13ee4b2ebe
kilted-andres <andres@trusted-entity.io>
2023-10-13 16:35:51 +0200

    fix: comments

--- backend/src/utils/generateAccount.ts
+++ backend/src/utils/generateAccount.ts
@@ -11,8 +11,6 @@ import * as Kilt from '@kiltprotocol/sdk-js'
  * @returns
  */
 export function generateAccount(mnemonic: string): Kilt.KiltKeyringPair {
-  // Currently, the default the key type used by the Kilt-team is "sr25519"
-  // transform to a U8 Array.
   const mnemonicToU8A = mnemonicToMiniSecret(mnemonic)
   const account = Kilt.Utils.Crypto.makeKeypairFromSeed(
     mnemonicToU8A,
