96c4acd7adcdabe73bd139f2dd5e51dfdf738f4a
Andrés <105802444+kilted-andres@users.noreply.github.com>
2023-10-16 10:24:59 +0200

    feat: updated key unification (#40)
    ## fixes KILTProtocol/ticket#2889
    from sr25519 to ed25519

--- backend/src/utils/generateAccount.ts
+++ backend/src/utils/generateAccount.ts
@@ -11,12 +11,10 @@ import * as Kilt from '@kiltprotocol/sdk-js'
  * @returns
  */
 export function generateAccount(mnemonic: string): Kilt.KiltKeyringPair {
-  // Currently, the default the key type used by the Kilt-team is "sr25519"
-  // transform to a U8 Array.
   const mnemonicToU8A = mnemonicToMiniSecret(mnemonic)
   const account = Kilt.Utils.Crypto.makeKeypairFromSeed(
     mnemonicToU8A,
-    'sr25519'
+    'ed25519'
   )
 
   return account
