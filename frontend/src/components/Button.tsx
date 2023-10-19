afb7ce8b3d8c93aa59d20361cd96073aa5475947
kilted-andres <andres@trusted-entity.io>
2023-10-19 18:22:20 +0200

    fix: button props

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,6 +4,8 @@ import styles from './Button.module.css'
 
 interface Props {
   children: React.ReactNode
+  disabled?: boolean
+  onClick?: () => void
 }
 
 export default function Button({ children, ...props }: Props) {
