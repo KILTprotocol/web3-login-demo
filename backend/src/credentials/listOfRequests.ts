21fc21eb90ce58aef1d9775b0cc7cbfc4a4be4b7
dudleyneedham <dudleyneedham1993@gmail.com>
2023-11-21 10:25:07 +0100

    feat: adding config infrmation and defaults

--- backend/src/credentials/listOfRequests.ts
+++ backend/src/credentials/listOfRequests.ts
@@ -2,23 +2,11 @@ import * as Kilt from '@kiltprotocol/sdk-js'
 
 import { CTYPE_HASH, REQUIRED_PROPERTIES, TRUSTED_ATTESTERS } from '../config'
 
+// Here you can set which type of credential (cType) your dApp will request users to login.
+// You can change it by importing a different one from the list.
+// The default is the Email CType by SocialKYC and SocialKYC as the Issuer
 // Establish which cTypes our dApp accepts and which attesters we trust:
 
-/**
- * Email Credential Type attested from SocialKYC.io
- */
-export const emailRequest: Kilt.IRequestCredentialContent = {
-  cTypes: [
-    {
-      cTypeHash:
-        '0x3291bb126e33b4862d421bfaa1d2f272e6cdfc4f96658988fbcffea8914bd9ac',
-      trustedAttesters: [
-        'did:kilt:4pehddkhEanexVTTzWAtrrfo2R7xPnePpuiJLC7shQU894aY'
-      ],
-      requiredProperties: ['Email']
-    }
-  ]
-}
 const trustedAttestersValues = TRUSTED_ATTESTERS.split(',')
 const requiredPropertiesValues = REQUIRED_PROPERTIES.split(',')
 
@@ -31,9 +19,9 @@ const trustedAttesters = trustedAttestersValues.map(
 )
 
 /**
- * Email Credential Type attested from SocialKYC.io
+ * Credential for users to configure default as SocialKYC Email Credential
  */
-export const requestedCType: Kilt.IRequestCredentialContent = {
+export const requestedCTypeForLogin: Kilt.IRequestCredentialContent = {
   cTypes: [
     {
       cTypeHash: CTYPE_HASH as `0x${string}`,
