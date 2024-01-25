78927b2ab8b3c9c8b70ddefb877a6e1c0a59f5f2
kilted-andres <andres@trusted-entity.io>
2024-01-25 16:58:59 +0100

    feat: make list of Requests apt for both chains

--- backend/src/credentials/listOfRequests.ts
+++ backend/src/credentials/listOfRequests.ts
@@ -1,19 +1,54 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
 
+import { WSS_ADDRESS } from '../config'
+
 // Establish which cTypes our dApp accepts and which attesters we trust:
 
+// DIDs and cType-Hashes are different depending on the blockchain:
+
 /**
- * Email Credential Type attested from SocialKYC.io on the Spiritnet (Production) KILT Blockchain.
+ * Email Credential Type attested from SocialKYC.io
  */
-export const emailRequest: Kilt.IRequestCredentialContent = {
-  cTypes: [
-    {
-      cTypeHash:
-        '0x3291bb126e33b4862d421bfaa1d2f272e6cdfc4f96658988fbcffea8914bd9ac',
-      trustedAttesters: [
-        'did:kilt:4pnfkRn5UurBJTW92d9TaVLR2CqJdY4z5HPjrEbpGyBykare'
-      ],
-      requiredProperties: ['Email']
-    }
-  ]
+export let emailRequest: Kilt.IRequestCredentialContent
+
+if (WSS_ADDRESS.includes('peregrine')) {
+  /**
+   * On the Peregrine (Test) KILT Blockchain.
+   */
+  emailRequest = {
+    cTypes: [
+      {
+        cTypeHash:
+          '0x3291bb126e33b4862d421bfaa1d2f272e6cdfc4f96658988fbcffea8914bd9ac',
+        trustedAttesters: [
+          'did:kilt:4pehddkhEanexVTTzWAtrrfo2R7xPnePpuiJLC7shQU894aY'
+        ],
+        requiredProperties: ['Email']
+      },
+      {
+        cTypeHash:
+          '0xae5bc64e500eb576b7b137288cec5d532094e103be46872f1ad54641e477d9fe',
+        trustedAttesters: [
+          'did:kilt:4pehddkhEanexVTTzWAtrrfo2R7xPnePpuiJLC7shQU894aY'
+        ],
+        requiredProperties: ['Email']
+      }
+    ]
+  }
+} else {
+  /**
+   * On the Spiritnet (Production) KILT Blockchain.
+   */
+  emailRequest = {
+    cTypes: [
+      {
+        cTypeHash:
+          '0x3291bb126e33b4862d421bfaa1d2f272e6cdfc4f96658988fbcffea8914bd9ac',
+        trustedAttesters: [
+          'did:kilt:4pnfkRn5UurBJTW92d9TaVLR2CqJdY4z5HPjrEbpGyBykare'
+        ],
+        requiredProperties: ['Email']
+      }
+    ]
+  }
 }
