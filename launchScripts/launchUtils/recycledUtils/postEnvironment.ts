33375274c218428f724d71f5f97c9df6f5bf8ea1
kilted-andres <andres@trusted-entity.io>
2024-05-22 15:10:18 +0200

    chore: clean up commented code

--- launchScripts/launchUtils/recycledUtils/postEnvironment.ts
+++ launchScripts/launchUtils/recycledUtils/postEnvironment.ts
@@ -2,7 +2,7 @@
  * Most of the utilities needed during the set-up that are also needed by the backend.
  * It is important that this utilities correlate so that the backend can use what was configured during set-up (launch).
  * To avoid duplicated code and discrepancies, it is better to import from the backend the utilities needed for the set-up.
- * That is what is done here.
+ * That is what is done here on the 'recycledUtils' folder.
  */
 
 //
@@ -18,9 +18,3 @@ import { fetchDidDocument } from '../../../backend/src/utils/fetchDidDocument'
 import { validateOurKeys } from '../../../backend/src/config'
 
 export { validateOurKeys, fetchDidDocument }
-
-// Problem, the config from the backend try to loads all the env-constants and throws if one is missing.
-// Functions that use the config from the backend:
-// - getApi
-// - fetchDidDocument
-// - validateOurKeys
